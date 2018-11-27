92f144511950eacadce860b10f7065e0a8c9e63f
Fernando Izquierdo <fer.izquierdo@gmail.com>
2018-11-27 23:27:17 +0000

    Add plot bar

--- make-over-monday/week48-cost-of-night-out/app.py
+++ make-over-monday/week48-cost-of-night-out/app.py
@@ -10,7 +10,6 @@ import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 
 df = pd.read_excel('cost.xlsx')
 
@@ -18,16 +17,56 @@ options = list(set(df.Item))
 default_options = ['2 Longdrinks', 'Big Mac', 'Taxi', 'Club entry']
 
 app.layout = html.Div([
-    html.H2('The cost of going out around the world'),
-    html.H4('Cities are sorted by the sum of the cost of all possible activities'),
+    html.H2('The cost of a night out in selected cities'),
+    html.H4('Choose activities and compare the cost of a night out around the world'),
     dcc.Checklist(
         id='features-options',
         options=[{'label': x, 'value': x} for x in options],
         values=default_options,
         labelStyle={'display': 'inline-block', 'margin': '6px'}),
-    html.Button(id='submit-button', n_clicks=0, children='Calculate'),
-    dcc.Graph(id='heatmap'),
+    html.Button(id='submit-button', n_clicks=0, children='Compare'),
+    html.Div([
+        html.Div([dcc.Graph(id='heatmap')], style={
+             'width': '50%', 'display': 'inline-block'}),
+        html.Div([dcc.Graph(id='bars')], style={
+            'width': '48%', 'display': 'inline-block'}),
+    ])
+
 ])
+
+# pivot for calculations
+df = df.pivot_table(index='City', columns='Item', values='Cost')
+
+
+def filter_activities(df, activities):
+    dfp = df[activities]
+    dfp['Total'] = dfp.sum(axis=1)
+    dfp = dfp.round(0).sort_values(by=['Total'])
+    cities = list(dfp.index)
+    return (dfp, cities)
+
+
+@app.callback(
+    dash.dependencies.Output('bars', 'figure'),
+    [dash.dependencies.Input('submit-button', 'n_clicks')],
+    [dash.dependencies.State('features-options', 'values')]
+)
+def update_graph(n_clicks, activities):
+    dfp, cities = filter_activities(df, activities)
+    return {
+        'data': [go.Bar(
+            x=list(dfp['Total']),
+            y=cities,
+            orientation='h',
+            marker=dict(color='rgba(190, 192, 213, 1)')
+        )],
+        'layout': go.Layout(
+            title='Total cost ($)',
+            paper_bgcolor='rgb(248, 248, 255)',
+            plot_bgcolor='rgb(248, 248, 255)',
+            yaxis=dict(automargin=True)
+        )
+    }
 
 
 @app.callback(
@@ -35,18 +74,19 @@ app.layout = html.Div([
     [dash.dependencies.Input('submit-button', 'n_clicks')],
     [dash.dependencies.State('features-options', 'values')]
 )
-def update_graph(n_clicks, values):
-    dfp = df.pivot_table(index='City', columns='Item', values='Cost')
-    dfp = dfp[options]
-    dfp['Total'] = dfp.sum(axis=1)
-    dfp = dfp.round(0).sort_values(by=['Total'])
-    cities = list(dfp.index)
+def update_graph(n_clicks, activities):
+    dfp, cities = filter_activities(df, activities)
     return {
-        'layout': go.Layout(title='Cost per activity in $'),
+        'layout': go.Layout(
+            title='Cost per activity ($)',
+            paper_bgcolor='rgb(248, 248, 255)',
+            yaxis=dict(automargin=True)
+        ),
         'data': [go.Heatmap(
-            x=values,
+            x=activities,
             y=cities,
-            z=dfp[values].values)]
+            z=dfp[activities].values,
+            colorscale='Reds')]
     }
 
 
